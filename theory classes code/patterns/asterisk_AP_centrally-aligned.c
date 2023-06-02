52cdd3a0dd862318c899cf4996da69e027947020
ammar <akidwai03@gmail.com>
2023-06-02 20:29:01 +0530

    02-06-2023 - 20:28

--- theory classes code/patterns/asterisk_AP_centrally-aligned.c
+++ theory classes code/patterns/asterisk_AP_centrally-aligned.c
@@ -1,3 +1,11 @@
+/*
+
+        1       
+    1   2   1   
+1   2   3   2   1
+
+*/
+
 #include <stdio.h>
 int main()
 {
@@ -5,6 +13,26 @@ int main()
 
     printf("Enter the number of lines for which pattern is to be printed : ");
     scanf("%d", &lines);
+
+    // line loop
+    for (int i = 0; i <= lines; i++)
+    {
+
+        // spaces loop
+        for (int j = 0; j < lines - i; j++)
+        {
+            printf("\t");
+        }
+
+        // character input loop - 1
+        for (int k = 1; k <= i; k++)
+        {
+            printf("%d\t", k);
+        }
+        printf("\n");
+        
+    }
+     
 
     return 0;
 }
