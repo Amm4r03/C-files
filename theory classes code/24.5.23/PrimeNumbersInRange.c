69f3b5ccaf2607e10e4c59195ef0583b461a2345
ammar <akidwai03@gmail.com>
2023-06-07 22:51:05 +0530

    finds primes between a range - still need to make code output numbers as well - currently only mentions count

--- theory classes code/24.5.23/PrimeNumbersInRange.c
+++ theory classes code/24.5.23/PrimeNumbersInRange.c
@@ -2,6 +2,54 @@
 int main()
 {
     int upper, lower, number;
-    printf("This program will check for prime numbers within a given range");
+    printf("This program will check for prime numbers within a given range\n");
+    
+    printf("Enter the lower limit : ");
+    scanf("%d", &lower);
+    printf("Enter the upper limit : ");
+    scanf("%d", &upper);
+    
+    int prime = 0;
+    int PrimeCount = 0;
+
+    for (int i = lower; i <= upper; i++)
+    {
+        if (i < 2)
+        {
+            prime = 0;
+        }
+
+        else if (i == 2)
+        {
+            prime = 1;
+            PrimeCount = PrimeCount + 1;
+        }
+
+        else if (i % 2 == 0)
+        {
+            prime = 0;
+        }
+
+        else
+        {
+            prime = 1;
+            for (int y = 3; y <= ((i/2)+1) ; y++)
+            {
+                if (i % y == 0)
+                {
+                    prime = 0;
+                    break;
+                }
+            }
+            if (prime == 1)
+            {
+                PrimeCount++;
+            }
+            
+        }
+    }
+    
+    printf("%d prime numbers have been found in between %d and %d", PrimeCount, lower, upper);
+
     return 0;
 }
