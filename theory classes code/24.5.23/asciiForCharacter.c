de6d405fb867ddec798905b6cc9cd40f91017565
ammar <akidwai03@gmail.com>
2023-06-08 22:41:54 +0530

    gives ascii code for character (newfile)

---
+++ theory classes code/24.5.23/asciiForCharacter.c
@@ -0,0 +1,11 @@
+// WAP to print ASCII number for a character
+
+#include <stdio.h>
+int main()
+{
+    printf("converts character input to its corresponding ASCII character\nEnter a character : ");
+    char input;
+    scanf("%c", &input);
+    printf("%d", input);
+    return 0;
+}
