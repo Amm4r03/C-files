db7c290f3401c434362f92690a8c6dc0c676b0cc
ammar <akidwai03@gmail.com>
2023-06-08 21:40:29 +0530

    updated logic to correct definition of armstrong no.

--- theory classes code/ArmstrongNumber.c
+++ theory classes code/ArmstrongNumber.c
@@ -4,22 +4,35 @@
 #include <math.h>
 int main ()
 {
-    int number, digit, sum, original;
-    sum=0;
+    int number, original;
+    double digit, sum;
+    int total_digits = 0;
+    sum = 0;
     original = 0;
     printf("This code will check if the input number is an armstrong number\n");
     printf("Enter a number : ");
     scanf("%d",&number);
     original = number;
-    while (number != 0)
+    int power = 0;
+
+    for (total_digits = 0; number != 0; total_digits++)
     {
         digit = number % 10;
-        digit = pow(digit,3);
-        sum = digit + sum;
         number = number / 10;
     }
 
-    printf("The sum of cubes of all digits is %d\n",sum);
+    number = original;
+    
+    for (int i = 0; i < total_digits; i++)
+    {
+        
+        digit = number % 10;
+        number = number / 10;
+        sum = pow(digit, total_digits) + sum;
+    }
+    
+
+    printf("The sum of cubes of all digits is %0.f\n",sum);
 
     if (sum == original)
     {
