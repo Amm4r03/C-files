52b688dc2da95f20eb1e891294b1df7b7f7be26f
ammar <akidwai03@gmail.com>
2023-06-09 17:10:26 +0530

    improving output readability

--- theory classes code/07.06.23/BinarySearch.c
+++ theory classes code/07.06.23/BinarySearch.c
@@ -61,7 +61,7 @@ int main()
     {
         if (i + 1 == size)
         {
-            printf("%d]", input_array[i]);
+            printf("%d]\n", input_array[i]);
         }
         else
         {
