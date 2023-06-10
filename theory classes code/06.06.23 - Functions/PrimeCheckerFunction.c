e073702ae78d069e7f28a33f635f4b6ebfbb68fe
ammar <akidwai03@gmail.com>
2023-06-10 11:34:13 +0530

    first version of PrimeStatus function

--- theory classes code/06.06.23 - Functions/PrimeCheckerFunction.c
+++ theory classes code/06.06.23 - Functions/PrimeCheckerFunction.c
@@ -1,9 +1,36 @@
 // WAF to check if a number is prime
 
 #include <stdio.h>
-int prime()
+int PrimeStatus(int number)
 {
+    int prime = 0;      // flag for determining if number is prime or not
+    if (number < 2)
+    {
+        prime = 0;
+        return prime;
+    }
+    if (number == 2)
+    {
+        prime = 1;
+        return prime;
+    }
+    if (number % 2 == 0)
+    {
+        prime = 0;
+        return prime;
+    }
 
+    prime = 1;  // assume number to be prime 
+
+    for (int i = 3; i < (number/2)+1; i++)
+    {
+        if (number % i == 0)
+        {
+            prime = 0;
+            return prime;
+        }
+    }
+    return prime;
 }
 
 int main()
