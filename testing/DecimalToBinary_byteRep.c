275865f18bc7fb7b084fd502c1dbc3dbfd00959e
ammar <akidwai03@gmail.com>
2023-06-16 09:32:55 +0530

    outputs binary number in boxes to rep bits

---
+++ testing/DecimalToBinary_byteRep.c
@@ -0,0 +1,67 @@
+// trying to output binary value of number in boxes to represent bytes
+
+// converts decimal numbers to binary
+
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter a number : ");
+    scanf("%d", &number);
+    int size;
+
+    
+    if ( number > 4294967295 )
+    {
+        printf("number too large to be represented in integer data type");
+        return 0;
+    }
+    else if (number < 255)
+    {
+        size = 8;       // sets size to 1 byte
+    }
+    else if (number > 16777215)
+    {
+        size = 32;      // sets size to 4 bytes (max for int data type)
+    }
+    else if (number > 65535)
+    {
+        size = 24;      // sets size to 3 bytes
+    }
+    else if (number > 255)
+    {
+        size = 16;      // sets size to 2 bytes
+    }
+    
+
+    int digits[size];
+    int digit;
+
+    for (int i = 0; i < size; i++)
+    {
+        digit = number % 2;
+        digits[i] = digit;
+        number = number / 2;
+    }
+
+    
+for (int i = size - 1; i >= 0; i--)
+{
+    printf("┌─┐");
+}
+printf("\n");
+
+
+for (int i = size - 1; i >= 0; i--)
+{
+    printf("│%d│", digits[i]);
+}
+
+printf("\n");
+
+for (int i = size - 1; i >= 0; i--)
+{
+    printf("└─┘");
+}
+    return 0;
+}
