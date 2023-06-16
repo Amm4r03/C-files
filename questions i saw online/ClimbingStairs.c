7946a45f93f4e4365702e60bc2fb1260c10c255c
ammar <akidwai03@gmail.com>
2023-06-16 08:43:40 +0530

    works on fibonacci sum of prev 2

--- questions i saw online/ClimbingStairs.c
+++ questions i saw online/ClimbingStairs.c
@@ -1,12 +1,11 @@
 // platform : leetcode
 // link : https://leetcode.com/problems/climbing-stairs/
 
-/*You are climbing a staircase. It takes n steps to reach the top.
+/* You are climbing a staircase. It takes n steps to reach the top.
 
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
  
-
 Example 1:
 
 Input: n = 2
@@ -23,8 +22,34 @@ Explanation: There are three ways to climb to the top.
 2. 1 step + 2 steps
 3. 2 steps + 1 step
  
-
 Constraints:
+1 <= n <= 45 */
 
-1 <= n <= 45*/
+#include <stdio.h>
 
+int climbStairs(int n)
+{
+  int i, ways = 1;
+  int fib[45];
+  fib[0] = 0;
+  fib[1] = 1;
+  for (i = 2; i < n; i = i + i)
+  {
+    fib[i] = fib[i-1] + fib[i-2];
+    ways = ways + fib[i];
+  }
+  return ways;
+}
+
+int main()
+{
+    printf("Enter a number : ");
+    int number;
+    scanf("%d", &number);
+    
+    int ok = climbStairs(number);
+
+    printf("%d", ok);
+
+    return 0;
+}
