e9264b5e0330650350e6364d1f9661cc5ca1cd60
ammar <akidwai03@gmail.com>
2023-06-18 17:11:47 +0530

    showing use of bitwise operators using binarytodecimal file

---
+++ vacation hhw/BitwiseOperators.c
@@ -0,0 +1,11 @@
+// WAP to illustrate the use of bitwise operators in C
+
+#include <stdio.h>
+
+int main()
+{
+    printf("this functions demonstrates the use of bitwise operators in C\n");
+    int a = 3, b = 2;
+    printf("%d", a&b);
+    return 0;
+}
