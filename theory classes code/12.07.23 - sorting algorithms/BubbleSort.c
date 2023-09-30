ee1206fab4f4f251dbad82299e19afc41ea1c7f1
ammar <akidwai03@gmail.com>
2023-09-30 12:24:03 +0530

    updated bubble sort code

---
+++ theory classes code/12.07.23 - sorting algorithms/BubbleSort.c
@@ -0,0 +1,18 @@
+// writing code for bubblesort
+
+#include <stdio.h>
+
+void BubbleSort(int, int);
+
+int main()
+{
+    printf("Enter the size of the array : ");
+    int size;
+    scanf("%d", &size);
+
+    int array[size];
+
+    printf("The array entered is as follows : ");
+
+    return 0;
+}
